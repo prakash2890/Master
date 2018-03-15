5ac47e97534543b1898d167e2ee1be141f54413a
prakash2890 <prakashraj2111@gmail.com>
2018-03-15 21:56:13 +0530

    First Day Changes

---
+++ MovieDB/Controllers/MoviesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieDB.Models;
+
+namespace MovieDB.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            Movie movie = new Movie()
+            {
+                Id = 1,
+                Name = "Swades"
+
+            };
+           return View(movie);
+           // return HttpNotFound();
+           // return new EmptyResult();
+          //  return RedirectToAction("Index", "Home", new { page=1,sortBy="Name"});
+        }
+    }
+}
